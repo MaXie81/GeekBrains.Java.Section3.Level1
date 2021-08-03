4b5fb69e8922e011ce3a7403790ebe8f4ba38637
KrakenUser <KrakenUser@yandex.ru>
2021-08-03 18:01:16 +0300

    Урок 4. Выполнение практического задания

--- src/main/java/geekbrains/java/section3/level1/webshop/controller/ShopController.java
+++ src/main/java/geekbrains/java/section3/level1/webshop/controller/ShopController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -31,7 +32,7 @@ public class ShopController {
         return "index";
     }
     @GetMapping("/all")
-    public String getAllProducts(Model model) {
+    public String getAllProducts(Model model, HttpServletRequest req) {
         model.addAttribute("listOfProducts", productService.getAllProducts());
         return "all_products";
     }
@@ -42,12 +43,22 @@ public class ShopController {
     }
     @GetMapping("/add")
     public String getPageAddProduct(Model model) {
-        model.addAttribute("id", productService.getId());
+        model.addAttribute("product", new Product(productService.getId(), "", 0.0f));
         return "add_product";
     }
     @PostMapping("/add")
     public String addProduct(@RequestParam long id, @RequestParam String title, @RequestParam float cost) {
         productService.addProduct(id, title, cost);
         return "redirect:/product/all";
+    }
+    @PostMapping("/add/inc")
+    public String increaseCost(Model model, @RequestParam long id, @RequestParam String title, @RequestParam float cost) {
+        model.addAttribute("product", new Product(id, title, cost + 10.0f));
+        return "add_product";
+    }
+    @PostMapping("/add/dec")
+    public String decreaseCost(Model model, @RequestParam long id, @RequestParam String title, @RequestParam float cost) {
+        model.addAttribute("product", new Product(id, title, cost - 10.0f));
+        return "add_product";
     }
 }
--- src/main/java/geekbrains/java/section3/level1/webshop/main/Product.java
+++ src/main/java/geekbrains/java/section3/level1/webshop/main/Product.java
@@ -2,10 +2,12 @@ package geekbrains.java.section3.level1.webshop.main;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
+@Setter
 @ToString
 public class Product {
     private long id;
