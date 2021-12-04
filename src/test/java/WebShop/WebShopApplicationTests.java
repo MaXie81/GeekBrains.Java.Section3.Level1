b5ad533218e827b0eb987311c8a703fa2741531c
KrakenUser <KrakenUser@yandex.ru>
2021-12-04 15:51:12 +0300

    Загружены все классы из Урок 3

---
+++ src/test/java/WebShop/WebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package WebShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
