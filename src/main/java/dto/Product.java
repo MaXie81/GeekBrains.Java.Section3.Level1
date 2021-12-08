0e918db797948d79d00f88408b16547c5de6fa6c
KrakenUser <KrakenUser@yandex.ru>
2021-12-08 20:50:05 +0300

    Добавлен файл импорта import.sql(создание таблицы PRODUCT), класс Product снабжен необходимыми аннотациями

--- src/main/java/dto/Product.java
+++ src/main/java/dto/Product.java
@@ -1,10 +1,24 @@
 package dto;
 
-public class Product {
-    private final long id;
-    private final String title;
-    private final int price;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "PRODUCT")
+public class Product {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "TITLE")
+    private String title;
+
+    @Column(name = "PRICE")
+    private int price;
+
+    public Product() {
+
+    }
     public Product(int id, String title, int price) {
         this.id = id;
         this.title = title;
